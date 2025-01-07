2f41a5b920b2cc75e9bd4d7cd9a22677dcfc73c7
karmanto supono <karmantosupono@karmantos-MacBook-Pro.local>
2025-01-08 05:57:44 +0700

    fix db error, now make retry

--- config.js
+++ config.js
@@ -3,13 +3,22 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 async function initDB() {
-    return await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT
-    });
+    while (true) {
+        try {
+            const connection = await mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE,
+                port: process.env.DB_PORT,
+            });
+            console.log('Database connection established successfully.');
+            return connection; 
+        } catch (error) {
+            console.error('Database connection failed. Retrying...', error.message);
+            await new Promise((resolve) => setTimeout(resolve, 5000)); 
+        }
+    }
 }
 
 module.exports = { initDB };
